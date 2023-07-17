50aa0d7e8223bffb3528cb7869a59f367b3bd90a
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-17 13:30:58 +0330

    Update sub.py

--- sub.py
+++ sub.py
@@ -1,41 +1,29 @@
 import os
 import telegram
+from telegram.ext import Updater, MessageHandler, Filters
 from pytube import YouTube
 from googletrans import Translator
 from pysrt import SubRipFile, SubRipItem, SubRipTime
 
 # Initialize the bot and the translator
 bot = telegram.Bot(token='YOUR_TELEGRAM_BOT_TOKEN')
+updater = Updater(token='YOUR_TELEGRAM_BOT_TOKEN')
+dispatcher = updater.dispatcher
 translator = Translator()
 
 # Function to download captions, translate them and send them via telegram
 def process_video(url, chat_id):
-    yt = YouTube(url)
-    captions = yt.captions.get_by_language_code('en')  # Assuming the video has English captions
+    # Your existing code...
 
-    if captions is None:
-        bot.send_message(chat_id=chat_id, text="No captions found for this video.")
-        return
+# Define a function to handle incoming messages
+def handle_message(update, context):
+    # Call process_video with the text of the message as the URL and the chat_id
+    process_video(update.message.text, update.message.chat_id)
 
-    # Download captions as srt
-    srt_captions = captions.generate_srt_captions()
-    with open('captions.srt', 'w') as f:
-        f.write(srt_captions)
+# Add the message handler to the dispatcher
+message_handler = MessageHandler(Filters.text, handle_message)
+dispatcher.add_handler(message_handler)
 
-    # Load the srt file and translate each item
-    subs = SubRipFile.open('captions.srt')
-    for item in subs:
-        translated_text = translator.translate(item.text, dest='fa').text
-        item.text = translated_text
-
-    # Save the translated srt file
-    subs.save('translated_captions.srt', encoding='utf-8')
-
-    # Send the file via telegram
-    bot.send_document(chat_id=chat_id, document=open('translated_captions.srt', 'rb'))
-
-    # Clean up
-    os.remove('captions.srt')
-    os.remove('translated_captions.srt')
-
-# TODO: Set up your bot to call process_video with the appropriate arguments when it receives a message
+# Start the bot
+updater.start_polling()
+updater.idle()
